4a18a540be8d3aad176a3ff317be7bd97751d259
Elston <elston1703@gmail.com>
2024-06-21 15:48:20 +0800

    OS1 integration and backend change

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,51 @@
-from flask import jsonify
-from src import create_app
+from flask import Flask, request, flash
+import numpy as np
+from datetime import datetime
+from pymongo.mongo_client import MongoClient
+from __init__ import envs
+from io import BytesIO
+import base64
+from PIL import Image
+import json
+from bson.objectid import ObjectId
+import requests
 
-# Entry point to run the Flask app
-app = create_app()
 
-@app.route('/')
-def wellcome():
-    return "<center><h1> Wellcome to Teepon<h1></center>"
 
-@app.errorhandler(404)
-def not_found(error):
-    return jsonify({'error' : 'Not found'}), 404
+def connect():
+    global db, client
+    """
+    code for connecting to the cluster
+    """
+    uri = envs['uri']
 
-@app.errorhandler(500)
-def internal_server_error(error):
-    return jsonify({'error' : 'Internal server error'}), 500
+    # Create a new client and connect to the server
+    client = MongoClient(uri)
+    # Send a ping to confirm a successful connection
+    try:
+        client.admin.command('ping')
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+    except Exception as e:
+        print(e)
+    db = client
+    return db
+
+app = Flask(__name__)
+connect()
+
+@app.route('/api/get_role', methods=['GET', 'POST'])
+def getRole():
+    db = connect().Main.users
+    print(db)
+    req = request.get_json()
+    uid = req['uid']
+    result = db.find({ "userid": uid })
+    result = result[0]
+    return {
+        'role': result['role']
+    }
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000, host='0.0.0.0')
+    connect()
+    app.run(debug=False, port=5000, host='0.0.0.0')
+
