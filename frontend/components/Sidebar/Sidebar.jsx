3577dc48aad97547853d0da1cb9020aba38e06e4
Partha Pratim Deka <parthapratimdeka129@gmail.com>
2024-06-21 13:37:02 +0530

    Prettier: sidebar.jsx

--- frontend/components/Sidebar/Sidebar.jsx
+++ frontend/components/Sidebar/Sidebar.jsx
@@ -1,109 +1,121 @@
-import React from 'react'
-import style from './sidebar.module.css'
+import React from "react";
+import style from "./sidebar.module.css";
 import { IoMdClose } from "react-icons/io";
 import { FiSearch, FiPlus } from "react-icons/fi";
 
-const Sidebar = ({hamburg, setHamburg}) => {
-
+const Sidebar = ({ hamburg, setHamburg }) => {
   const outings = [
     {
-      'name' : 'Weekend Outings',
-      'desc' : 'Weekend party near the hills'
+      name: "Weekend Outings",
+      desc: "Weekend party near the hills",
     },
     {
-      'name' : 'Weekend Outings',
-      'desc' : 'Weekend party near the hills'
+      name: "Weekend Outings",
+      desc: "Weekend party near the hills",
     },
     {
-      'name' : 'Weekend Outings',
-      'desc' : 'Weekend party near the hills'
+      name: "Weekend Outings",
+      desc: "Weekend party near the hills",
     },
     {
-      'name' : 'Weekend Outings',
-      'desc' : 'Weekend party near the hills'
+      name: "Weekend Outings",
+      desc: "Weekend party near the hills",
     },
     {
-      'name' : 'Weekend Outings',
-      'desc' : 'Weekend party near the hills'
+      name: "Weekend Outings",
+      desc: "Weekend party near the hills",
     },
     {
-      'name' : 'Weekend Outings',
-      'desc' : 'Weekend party near the hills'
+      name: "Weekend Outings",
+      desc: "Weekend party near the hills",
     },
     {
-      'name' : 'Weekend Outings',
-      'desc' : 'Weekend party near the hills'
+      name: "Weekend Outings",
+      desc: "Weekend party near the hills",
     },
     {
-      'name' : 'Weekend Outings',
-      'desc' : 'Weekend party near the hills'
+      name: "Weekend Outings",
+      desc: "Weekend party near the hills",
     },
     {
-      'name' : 'Weekend Outings',
-      'desc' : 'Weekend party near the hills'
+      name: "Weekend Outings",
+      desc: "Weekend party near the hills",
     },
     {
-      'name' : 'Weekend Outings',
-      'desc' : 'Weekend party near the hills'
+      name: "Weekend Outings",
+      desc: "Weekend party near the hills",
     },
     {
-      'name' : 'Weekend Outings',
-      'desc' : 'Weekend party near the hills'
+      name: "Weekend Outings",
+      desc: "Weekend party near the hills",
     },
     {
-      'name' : 'Weekend Outings',
-      'desc' : 'Weekend party near the hills'
+      name: "Weekend Outings",
+      desc: "Weekend party near the hills",
     },
     {
-      'name' : 'Weekend Outings',
-      'desc' : 'Weekend party near the hills'
+      name: "Weekend Outings",
+      desc: "Weekend party near the hills",
     },
     {
-      'name' : 'Weekend Outings',
-      'desc' : 'Weekend party near the hills'
+      name: "Weekend Outings",
+      desc: "Weekend party near the hills",
     },
     {
-      'name' : 'Weekend Outings',
-      'desc' : 'Weekend party near the hills'
+      name: "Weekend Outings",
+      desc: "Weekend party near the hills",
     },
     {
-      'name' : 'Weekend Outings',
-      'desc' : 'Weekend party near the hills'
-    }
-    
-  ]
+      name: "Weekend Outings",
+      desc: "Weekend party near the hills",
+    },
+  ];
 
   return (
-    <div className={`${style.sidebar} ${!hamburg ? style.hide : ''}`}>
-      <div className={style.close} onClick={()=>{setHamburg(false)}}><IoMdClose /></div>
+    <div className={`${style.sidebar} ${!hamburg ? style.hide : ""}`}>
+      <div
+        className={style.close}
+        onClick={() => {
+          setHamburg(false);
+        }}
+      >
+        <IoMdClose />
+      </div>
       <div className={style.upper_dock}>
         <h1 className={style.outings}>Outings</h1>
         <div className={style.searchbar}>
-          <FiSearch className={style.search_icon}/>
-          <input type="text" name="search" id="search" className={style.input} placeholder='Search' />
+          <FiSearch className={style.search_icon} />
+          <input
+            type="text"
+            name="search"
+            id="search"
+            className={style.input}
+            placeholder="Search"
+          />
         </div>
       </div>
 
       <div className={style.middle_dock}>
         <ul className={style.outings}>
-          {outings.map((item)=>{
-            return <li className={style.outing}>
-              <div className={style.title}>{item.name}</div>
-              <div className={style.desc}>{item.desc}</div>
-            </li>
+          {outings.map((item) => {
+            return (
+              <li className={style.outing}>
+                <div className={style.title}>{item.name}</div>
+                <div className={style.desc}>{item.desc}</div>
+              </li>
+            );
           })}
         </ul>
       </div>
 
-
       <div className={style.lower_dock}>
         <div className={style.profile}></div>
         <button className={style.new}>
-          <FiPlus className={style.svg}/> New
+          <FiPlus className={style.svg} /> New
         </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
