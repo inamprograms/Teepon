fe49a26402888eaf5914f90111069bbf1536cde7
Partha Pratim Deka <parthapratimdeka129@gmail.com>
2024-06-28 09:48:05 +0530

    Merge branch 'main' of https://github.com/Elstuhn/Teepon

---
+++ backend/models.py
@@ -0,0 +1,29 @@
+# app/models.py
+from .extensions import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'email': self.email
+        }
+
+    @classmethod
+    def get_all(cls):
+        return cls.query.all()
+
+    @classmethod
+    def get_by_id(cls, user_id):
+        return cls.query.get(user_id)
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
